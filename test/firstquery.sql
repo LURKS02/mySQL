dd33ab5b470bc56b9c961b9b4eebc89540537c50
LURKS02 <thecrooks2@gmail.com>
2020-06-09 17:36:36 +0900

    Add files via upload
    sql test

---
+++ test/firstquery.sql
@@ -0,0 +1,5 @@
+show tables;
+desc offer;
+#insert into offer (name, email, text) values ('user', 'user@email.com', 'text');
+#insert into offer (name, email, text) values ('user2', 'user2@email.com', 'text2');
+select * from offer LIMIT 0, 1000
